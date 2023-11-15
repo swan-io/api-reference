0bdc217749312c0b1393f128632d6850104dd561
bloodyowl <bloodyowl@icloud.com>
2023-11-15 14:29:43 +0100

    Revamp

---
+++ docusaurus.config.js
@@ -0,0 +1,81 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/oceanicNext");
+const path = require("path");
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Swan Docs",
+  tagline: "Onboarding & Banking clients for Swan",
+  url: "https://api-reference.swan.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "swan-io", // Usually your GitHub org/user name.
+  projectName: "api-reference", // Usually your repo name.
+  themeConfig: {
+    algolia: {
+      appId: "PWGO6EETLK",
+      apiKey: "55d34883c659e60e4d4d625f78c01fbc",
+      indexName: "swan-ioio",
+      contextualSearch: true,
+      searchParameters: {},
+      searchPagePath: "search",
+      externalUrlRegex:
+        "api-reference\\.swan\\.io|docs\\.swan\\.io|incomparable-tiramisu-91a96a\\.netlify\\.app|swan-io\\.github\\.io",
+    },
+    navbar: {
+      title: "API reference",
+      logo: {
+        alt: "Swan",
+        src: "img/logo-swan.svg",
+        width: 70,
+      },
+    },
+    // footer: {
+    //   // style: "dark",
+    //   copyright: `Copyright © ${new Date().getFullYear()} Swan`,
+    // },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
+  scripts: [],
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/swan-io/api-reference/edit/main/docs/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      "@graphql-markdown/docusaurus",
+      {
+        schema: path.join(
+          process.cwd(),
+          "scripts/graphql/dist/partner-schema.gql"
+        ),
+        rootPath: "./docs",
+        baseURL: "",
+        loaders: {
+          GraphQLFileLoader: "@graphql-tools/graphql-file-loader",
+        },
+      },
+    ],
+  ],
+};
